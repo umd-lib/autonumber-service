8713e0d5b509ffa983b91f270520552f6e494296
Peter Eichman <peichman@umd.edu>
2016-04-06 15:24:03 -0400

    LIBITD-397. Validate name and repository parameters.
    Require the name and repository parameters to the batch number form. Added a test of this validation to the batch controller tests.
    
    https://issues.umd.edu/browse/LIBITD-397

--- app/controllers/batch_controller.rb
+++ app/controllers/batch_controller.rb
@@ -3,25 +3,33 @@ class BatchController < ApplicationController
   end
 
   def create
-    quantity = batch_params[:quantity].to_i
     respond_to do |format|
-      if quantity > 0
+      errors = check_params
+      if errors.empty?
         stats = AutoNumber.create_batch(quantity, auto_number_params)
         format.html { redirect_to :batch, flash: stats }
       else
-        format.html { redirect_to :batch, flash: { error: 'Quantity must be greater than 0' } }
+        format.html { redirect_to :batch, flash: { errors: errors } }
       end
     end
   end
 
   private
 
+    def check_params
+      errors = []
+      errors.push('Quantity must be greater than 0') unless quantity > 0
+      errors.push('Name is required') if auto_number_params[:name_id].nil?
+      errors.push('Repository is required') if auto_number_params[:repository_id].nil?
+      errors
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def auto_number_params
       params.require(:batch).permit(:entry_date, :name_id, :repository_id)
     end
 
-    def batch_params
-      params.require(:batch).permit(:quantity)
+    def quantity
+      params.require(:batch).permit(:quantity)[:quantity].to_i
     end
 end
