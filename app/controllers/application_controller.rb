3c5ff1c8ba5230b501348ab7659734e2f65aca58
Peter Eichman <peichman@umd.edu>
2017-05-03 14:19:38 -0400

    LIBITD-429. Require the authenticated CAS user to be listed in the app database.
    Provide a rake task to add users, which will be used for (at least) the initial user.
    
    https://issues.umd.edu/browse/LIBITD-429

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action CASClient::Frameworks::Rails::Filter
+  before_action :whitelist_cas_user
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+    def whitelist_cas_user
+      render status: 403, text: 'Unauthorized' unless User.find_by cas_directory_id: session[:cas_user]
+    end
 end
