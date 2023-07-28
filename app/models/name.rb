3c7af4f8be75d43a18a19a24c6ccace61f153e6b
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2023-07-28 13:47:31 -0400

    LIBITD-2345: Correcting Style/StringLiterals offense
    https://umd-dit.atlassian.net/browse/LIBITD-2345

--- app/models/name.rb
+++ app/models/name.rb
@@ -7,6 +7,6 @@ class Name < ApplicationRecord
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "id", "initials", "updated_at"]
+    ['created_at', 'id', 'initials', 'updated_at']
   end
 end
